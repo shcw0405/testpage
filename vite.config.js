9221e7e0b99297684f7b6fdbb8a9ccc822e593ac
shcw0405 <112615230+shcw0405@users.noreply.github.com>
2025-03-10 04:33:44 +0000

    Deploying to gh-pages from @ shcw0405/testpage@18363fd2b898369b584a62675ba30cbfcefdb01c 🚀

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/testpage/", // 设置正确的base路径，对应GitHub Pages的仓库名
-});
